115f4b63da8624c8ef1099daabf09b27bdd7fa14
Laura Moldovan <laura.duduma@gmail.com>
2021-09-24 14:30:35 +0200

    1

---
+++ js/skript.js
@@ -0,0 +1,12 @@
+/*(20 points) Create the function calculateInvoice() in Javascript that returns 
+the invoice for one customer (how much they spent). 
+The parameters of the function should be starterPrice, maindishPrice, dessertPrice and beveragePrice. 
+The function will return the sum of these four parameters.
+
+(10 points) Use the prices on your menu to create four variables that you will 
+pass to the function calculateInvoice() as arguments. 
+Print the result on the console. 
+Note: You should do it manually since we didn't learn how to take values from html elements yet.
+
+(10 points) Calculate at least 3 invoices using combinations of different dishes on your menu 
+and print them on the console (using one dish of each category).*/
